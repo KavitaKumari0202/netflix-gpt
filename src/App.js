d65e50d77cf79790df556b74704110fe75119429
Kavita Kumari <kavi456ta@gmail.com>
2026-04-16 17:28:51 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p className='font-bold text-green-200'> Let'sbegin coding our project</p>
+    </div>
+  );
+}
+
+export default App;
